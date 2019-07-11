5e09514ab5911af9fa9f39da7cf5145c093a4ffe
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 16:11:15 -0500

    Create While.java

---
+++ While/While.java
@@ -0,0 +1,18 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		int contador;
+		
+		contador=6;
+		
+		while(contador<6) {
+			System.out.println("Contador: "+contador);
+			contador=contador+1;			
+		}
+
+		
+	}
+	
+}
