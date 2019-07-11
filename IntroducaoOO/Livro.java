67ae1ba15aed3648f8c1ec2128eba7b0495ec0bc
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 16:00:35 -0500

    Create Livro.java

---
+++ IntroducaoOO/Livro.java
@@ -0,0 +1,37 @@
+public class Livro {
+	
+	private int codigo;
+	private String titulo;
+	private String autor;
+	private String editora;
+	private int edicao;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public int getEdicao() {
+		return edicao;
+	}
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
