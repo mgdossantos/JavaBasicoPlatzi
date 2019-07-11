25b7a388727dc6cce5a00a4e0b9843e7cea5842f
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 15:46:39 -0500

    Create Aprofundando.java
    Aula  onde foram apresentandos mas tipos de dados.

---
+++ AprofundandoTiposdeDados/Aprofundando.java
@@ -0,0 +1,24 @@
+
+public class Aprofundando {
+
+	public static void main(String[] args) {
+		// armazena uma letra
+		char cadeira;
+		cadeira='o';
+		System.out.println("Qual o status da cadeira? "+cadeira);
+		
+		cadeira='l';
+		System.out.println("Qual o status da cadeira? "+cadeira);
+		
+		boolean lampada;
+		
+		lampada=true;
+		System.out.println("Qual o status da lampada? "+lampada);
+		lampada=false;
+		System.out.println("Qual o status da lampada? "+lampada);
+
+
+
+	}
+
+}
