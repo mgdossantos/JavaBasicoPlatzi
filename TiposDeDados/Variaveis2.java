e03d4660d30af30e8a442ce1de1e052025e9c5cd
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 18:07:01 -0300

    Add files via upload

---
+++ TiposDeDados/Variaveis2.java
@@ -0,0 +1,22 @@
+
+public class Variaveis2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int idade=35;
+		long habitantes=250000;
+		
+		double saldo;
+		float saldo2;
+		
+		//para declarar float preciso colocar f no final
+		saldo=250.60;
+		saldo2=350.50f;
+		
+		System.out.println("Saldo 1: "+saldo);
+		System.out.println("Saldo 2: "+saldo2);
+
+	}
+
+}
