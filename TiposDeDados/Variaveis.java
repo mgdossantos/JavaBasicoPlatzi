969a847790e343adf98a77f150e4b73fc4d2cece
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 16:06:48 -0500

    Create Variaveis.java

---
+++ TiposDeDados/Variaveis.java
@@ -0,0 +1,22 @@
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		// mostra idade e nome
+		int idade;
+		String nome;
+		
+		nome="Marcela Goncalves Santos";
+		idade=35;
+		
+		System.out.println("Nome: "+nome);
+		System.out.println("Idade: "+idade);
+		
+		idade=36;
+		System.out.println("Modificando variavel");
+		System.out.println("Idade: "+idade);
+
+
+	}
+
+}
