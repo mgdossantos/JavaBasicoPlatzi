43d8062cb762345a9daca6f70c9c7f00f48ce40c
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 17:52:47 -0300

    Add files via upload
    Desafio da aula Estrutura Basica

---
+++ EstruturaBasica/DesafioAulaEstruturaBasica.java
@@ -0,0 +1,10 @@
+
+public class DesafioAulaEstruturaBasica {
+
+	public static void main(String[] args) {
+		// crie um programa em Java que mostre na tela seu nome.
+		System.out.println("Marcela Santos");
+		
+	}
+
+}
