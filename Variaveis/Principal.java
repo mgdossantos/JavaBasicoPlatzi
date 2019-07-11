5855684f3e49bba9cf1b24c6bae4b1e7b76dc210
Marcela Santos <tchellita.engcomp@gmail.com>
2019-07-11 16:07:47 -0500

    Create Principal.java

---
+++ Variaveis/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int idade;
+		String nome;
+		
+		idade=35;
+		nome="Marcela Santos";
+		
+		System.out.println("Idade: "+idade);
+		System.out.println("Nome :"+nome);
+		
+
+	}
+
+}
